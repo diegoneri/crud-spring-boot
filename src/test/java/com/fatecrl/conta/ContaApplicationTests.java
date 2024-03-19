ffa75c19a0f0df9e5e9f4866f022f3de04522441
Diego Neri <diego.neri@alpar.com.br>
2024-03-19 18:58:32 -0300

    Estrutura do start.spring.io

---
+++ src/test/java/com/fatecrl/conta/ContaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatecrl.conta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
